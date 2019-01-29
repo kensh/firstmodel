b6b7bcef8f9c3841b0151f429508fc35be12817b
kensuke shimokawa <kenngkenng.gml@gmail.com>
2019-01-29 23:46:09 +0900

    nope

--- exec.sh
+++ exec.sh
@@ -1,6 +1,6 @@
 LF=$'\\\x0A'
- maude -no-wrap search | sed 's/}/}'"$LF$LF"'/g' | sed 's/])/])'"$LF"'/g' | sed 's/))/))'"$LF"'/g' | sed 's/mt)/mt)'"$LF"'/g'
-# maude -no-wrap check maude -no-wrap check | sed 's/{/'"$LF$LF"'{/g' | sed 's/}/}'"$LF"'/g' | sed 's/])/])'"$LF"'/g' | sed 's/))/))'"$LF"'/g' | sed 's/mt)/mt)'"$LF"'/g'
+ # maude -no-wrap search | sed 's/}/}'"$LF$LF"'/g' | sed 's/])/])'"$LF"'/g' | sed 's/))/))'"$LF"'/g' | sed 's/mt)/mt)'"$LF"'/g'
+ maude -no-wrap check maude -no-wrap check | sed 's/{/'"$LF$LF"'{/g' | sed 's/}/}'"$LF"'/g' | sed 's/])/])'"$LF"'/g' | sed 's/))/))'"$LF"'/g' | sed 's/mt)/mt)'"$LF"'/g'
 
 # maude search
 # maude check
